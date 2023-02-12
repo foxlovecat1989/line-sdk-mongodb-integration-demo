f7924f579782d453c88b6973c70aad5cd4964db2
chenhuangkai <huang.kai@tpisoftware.com>
2023-02-12 20:44:49 +0800

    init - commit #1 setup a Sprint Boot project 使用 Spring initializr建立 maven專案, spring boot version 3.0.2

---
+++ src/main/java/com/ed/app/linesdkmongodbintegrationdemo/LineSdkMongodbIntegrationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ed.app.linesdkmongodbintegrationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineSdkMongodbIntegrationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LineSdkMongodbIntegrationDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ed/app/linesdkmongodbintegrationdemo/LineSdkMongodbIntegrationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ed.app.linesdkmongodbintegrationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineSdkMongodbIntegrationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
